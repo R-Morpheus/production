9fff1af559ce813421010d8ffdf76a4c2ba78061
rashid <gagach16rus@gmail.com>
2023-10-23 17:41:03 +0300

    add: eslint, settings config, remove errors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
-import React, {Suspense} from 'react';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {SideBar} from "widgets/SideBar";
-import './styles/index.scss'
+import React, { Suspense } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { SideBar } from 'widgets/SideBar';
+import './styles/index.scss';
 
 const App = () => {
-    const {theme} = useTheme();
+    const { theme } = useTheme();
     return (
-      <Suspense fallback={''}>
-        <div className={classNames('app', {}, [theme])}>
-          <Navbar/>
-          <div className='content-page'>
-            <SideBar/>
-            <AppRouter/>
-          </div>
-        </div>
-      </Suspense>
+        <Suspense fallback="">
+            <div className={classNames('app', {}, [theme])}>
+                <Navbar />
+                <div className="content-page">
+                    <SideBar />
+                    <AppRouter />
+                </div>
+            </div>
+        </Suspense>
 
     );
 };
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -13,7 +13,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
 const Button: FC<ButtonProps> = (props) => {
   const {className, children, theme,...otherProps} = props
   return (
-    <button className={classNames(cls.Button , {}, [className, cls[theme]])} {...otherProps}>
+    <button type='button' className={classNames(cls.Button , {}, [className, cls[theme]])} {...otherProps}>
       {children}
     </button>
   );
