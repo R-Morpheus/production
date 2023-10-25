41dcf758f597a20533f3fbb684187e5ae004e528
rashid <gagach16rus@gmail.com>
2023-10-25 22:18:19 +0300

    add: BundleAnalyzerPlugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,9 @@ module.exports = {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     indent: [2, 2],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      2, { extensions: ['.js', '.jsx', '.tsx'] }
+    ],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
@@ -41,7 +43,7 @@ module.exports = {
     'no-underscore-dangle': 'off',
 	  'no-mixed-spaces-and-tabs': 'off',
 	  "i18next/no-literal-string": ['error', {markupOnly: true}],
-	  'max-len': ['error', {ignoreComments: true}]
+	  'max-len': ['error', {ignoreComments: true, code: 120}]
   },
   globals: {
     __IS_DEV__: true,
