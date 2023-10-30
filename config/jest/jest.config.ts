d509f0272973754f3d104ec2078998752bf3d24f
rashid <gagach16rus@gmail.com>
2023-10-30 15:53:16 +0300

    refactor: project; add: storybook, loki

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,3 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 import path from 'path';
 
 export default {
